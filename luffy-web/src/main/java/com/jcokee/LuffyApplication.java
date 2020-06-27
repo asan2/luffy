c9e3bb4267a8a21c743f89f37d16684bac6314ab
jaakko <jcokee@163.com>
2020-06-27 22:24:52 +0800

    集成springboot

--- luffy-web/src/main/java/com/jcokee/LuffyApplication.java
+++ luffy-web/src/main/java/com/jcokee/LuffyApplication.java
@@ -1,5 +1,12 @@
 package com.jcokee;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class LuffyApplication {
 
+  public static void main(String[] args) {
+    SpringApplication.run(LuffyApplication.class, args);
+  }
 }
